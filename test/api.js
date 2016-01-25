b4bd331371a365bc411b28b7244479329fd27f99
Trygve Lie <github@trygve-lie.com>
2016-01-26 00:38:33 +0100

    added ping/pong events and started adding tests

--- test/api.js
+++ test/api.js
@@ -1,29 +1,44 @@
 "use strict";
 
-const stream = require('readable-stream'),
-      Client = require('../lib/api.js');
+const Client = require('../').Api,
+	  stream = require('readable-stream'),
+	  nock = require('nock'),
+      tap = require('tap');
 
 
 
-
-var writable = new stream.Writable({
-  objectMode : true,
-  write: function(chunk, encoding, next) {
-    console.log(chunk);
-    next();
-  }
+tap.test('options object not provided - result should throw assert error', (t) => {
+    t.throws(() => {
+        let client = new Client();
+    }, new Error('you must pass an options object'));
+    t.end();
 });
 
 
+tap.test('key parameter not provided - result should throw assert error', (t) => {
+    t.throws(() => {
+        let client = new Client({});
+    }, new Error('you must pass an api key on the options object'));
+    t.end();
+});
+
+
+
+
+/*
 var foo = new Client();
 
-// foo.getCharger('NOR_01523').pipe(writable);
-/*
+
 foo.getCharger('NOR_01523', function (error, body) {
     console.log(body);
 });
-*/
+
 
 foo.getCharges(function (error, body) {
     console.log(body);
 });
+
+foo.getCountry('NOR', function (error, body) {
+    console.log(body);
+});
+*/
--- test/stream.js
+++ test/stream.js
@@ -1,7 +1,63 @@
 "use strict";
 
-const stream = require('readable-stream'),
-	  client = require('../lib/stream.js');
+const Client = require('../').Stream,
+      stream = require('readable-stream'),
+      tap = require('tap'),
+      ws = require('ws');
+
+
+
+tap.test('options object not provided - result should throw assert error', (t) => {
+    t.throws(() => {
+        let foo = new Client();
+    }, new Error('you must pass an options object'));
+    t.end();
+});
+
+
+tap.test('key parameter not provided - result should throw assert error', (t) => {
+    t.throws(() => {
+        let foo = new Client({});
+    }, new Error('you must pass an api key on the options object'));
+    t.end();
+});
+
+
+
+
+
+
+
+
+/*
+
+const wss = new ws.Server({ port: 8080 });
+
+wss.on('connection', (socket) => {
+
+    let pingThreshold = 2,
+        pingFrequency = 1000 * 30;
+
+    socket.pingSent = 0;
+    socket.pingInterval = setInterval(() => {
+        if (socket.readyState !== 1)
+            return;
+        if (socket.pingSent >= pingThreshold)
+            return socket.close();
+        socket.ping();
+        socket.pingSent++;
+    }, pingFrequency);
+
+    socket.on('pong', () => {
+        socket.pingSent = 0;
+    });
+
+    socket.on('close', () => {
+        clearInterval(socket.pingInterval);
+    });
+
+    socket.send(JSON.stringify({foo:'bar'}));
+});
 
 
 
@@ -13,21 +69,26 @@ var writable = new stream.Writable({
   }
 });
 
+
 writable.on('error', (error) => {
-	console.log(error);
+    console.log(error);
 });
 
 
-var foo = new client({key : ''});
-foo.on('connect', () => {
-	console.log('fooo');
+var cli = new client({key : ''});
+cli.on('connection', () => {
+    console.log('connect');
 });
 
-foo.pipe(writable);
-
-
-/*
-foo.on('m', (msg) => {
-	console.log(msg);
+cli.on('close', (msg) => {
+    console.log('close', msg);
 });
+
+cli.on('error', (error) => {
+    console.log('error', error);
+});
+
+
+cli.pipe(writable);
+
 */
